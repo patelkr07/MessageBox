e7f5013d77c285edb8ad80810176c14f9510427c
apizarras <angelalee.p@gmail.com>
2019-06-20 21:44:12 -0500

    working out dynamically displaying messages

--- public/messages.js
+++ public/messages.js
@@ -1,4 +1,55 @@
 $(document).ready(function() {
+
+    let $newItemInput = $("input.new-item");
+    var $postContainer = $(".post-container");
+    const posts = [];
+
+    getPosts();
+
+    function initializeRows(posts) {
+        // console.log("this is row data" + posts);
+        $postContainer.empty();
+        const rowsToAdd = [];
+        for(i=0; i<posts.length; i++) {
+            // console.log("loop of posts: " + posts[0]);
+            rowsToAdd.push(createNewRow(posts[i]));
+        }
+        $postContainer.prepend(rowsToAdd);
+    }
+
+    function getPosts() {
+        console.log("getPosts function is running");
+        $.get("/get/messages", function(dbPost) {
+            // console.log("this is data: " + data);
+            console.log("db: " + dbPost);
+            let posts = dbPost;
+            initializeRows(posts);
+        });
+    };
+
+    function createNewRow(dbPost) {
+        // console.log("create new row: " + dbPost);
+        let $newInputRow = $(
+            [
+                //dynamic html in quotes here
+            "<div class='card' style='width: 18rem;'>",
+            "<i class='fas fa-sms' aria-hidden='true'></i>",
+            "<div class='card-body'>",
+              "<h5 class='card-title'>From Phone Number</h5>",
+              "<p class='card-text'>",
+              "<span>",
+              dbPost.body,
+            "</span>",
+              "</p>",
+              "<a href='#' class='btn btn-primary'>Send Message</a>",
+            "</div>",
+          "</div>"
+            ].join("")
+    );
+    $newInputRow.data("post", dbPost);
+    return $newInputRow;
+};
+
     $('#plivo').click(function() {
         let dst = $("#dst").val().trim();
         console.log("dst phone: " + dst);
@@ -42,13 +93,6 @@ console.log(twilioData);
     
     });
     
-    
-
-
-
-
-
-
 // modal code from getbootstrap.com
     $('#Modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
@@ -63,6 +107,3 @@ console.log(twilioData);
 
 
 // let socket = io();
-
-
-
